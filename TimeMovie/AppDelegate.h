69e341bba87a5ce7b8e73955bc63e7fe541373e6
cdf <cdf@cdfdeMacBook-Pro.local>
2015-08-19 13:45:14 +0800

    Initial Commit

---
+++ TimeMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeMovie
+//
+//  Created by cdf on 15/8/19.
+//  Copyright (c) 2015年 cdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeMovie
+//
+//  Created by cdf on 15/8/19.
+//  Copyright (c) 2015年 cdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
